e9fa394a34a059b29629fd0c427afbc0f681469a
RanaHashim <RanaHashim@users.noreply.github.com>
2024-06-23 20:31:44 +0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,7 +79,7 @@ const Navbar = () => {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            d="M6 18L18 6M6 6112 12"
+            d="M6 18L18 6M6 6l12 12"
             />
           </svg>
         ):<svg
