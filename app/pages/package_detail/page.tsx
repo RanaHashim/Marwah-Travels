b91df2a6c437d6fa04f730effee86a5516b9032e
RanaHashim <RanaHashim@users.noreply.github.com>
2024-10-02 02:11:54 +0500

    Update page.tsx

--- app/pages/package_detail/page.tsx
+++ app/pages/package_detail/page.tsx
@@ -221,9 +221,9 @@ export default function PackageDetail() {
                                                 <img
                                                     className="rounded-xl"
                                                     src={
-                                                        (FILE_BASE_URL + packageToView.hotel_makkah_image ??
+                                                        (FILE_BASE_URL + packageToView.hotel_makkah_image) ??
                                                         "" ??
-                                                        "")
+                                                        ""
                                                     }
                                                     alt="Data Image"
                                                     width={400}
